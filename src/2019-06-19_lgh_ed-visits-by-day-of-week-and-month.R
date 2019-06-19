2692c89695a9dd2000304f612019c11d9a3ba458
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2019-06-19 13:09:58 -0700

    beeswarms and boxplots

--- src/2019-06-19_lgh_ed-visits-by-day-of-week-and-month.R
+++ src/2019-06-19_lgh_ed-visits-by-day-of-week-and-month.R
@@ -3,18 +3,23 @@
 #' title: "LGH ED visits by day of week"
 #' author: "Nayef Ahmad"
 #' date: "2019-06-19"
-#' output: html_document
+#' output: 
+#'   html_document: 
+#'     keep_md: yes
+#'     code_folding: hide
 #' ---
 #' 
 
-
+#+ libraries, message = FALSE 
 library(tidyverse)
 library(here)
 library(odbc)
 library(DBI)
 library(lubridate)
 library(ggbeeswarm)
+library(DT)
 
+#+ analysis 
 # 1) set up database connections and import functions: -----------
 source(here::here("src", 
                   "setup-denodo_function.R"))
@@ -37,16 +42,31 @@ df2.ed_visits_cleaned <-
          year = year(date), 
          years_from_2017 = year - 2017) %>% 
   
+  # fiddle with factors: 
+  mutate(weekday = fct_relevel(weekday, 
+                               levels = c("Monday", 
+                                          "Tuesday", 
+                                          "Wednesday", 
+                                          "Thursday", 
+                                          "Friday",
+                                          "Saturday", 
+                                          "Sunday")), 
+         years_from_2017 = as.factor(years_from_2017), 
+         year = as.factor(year)) %>% 
+  
   rename(ed_visits = value) %>% 
   
   select(date, 
          years_from_2017, 
          month, 
+         year, 
          weekday, 
          ed_visits)
 
-str(df2.ed_visits_cleaned)
+# str(df2.ed_visits_cleaned)
 
+df2.ed_visits_cleaned %>% 
+  datatable()
 
 
 # 3) plots: ------------
@@ -61,14 +81,52 @@ df2.ed_visits_cleaned %>%
       
 
 df2.ed_visits_cleaned %>% 
-  # group_by(years_from_2017, 
-  #          month, 
-  #          weekday) %>% 
-  # summarise(avg_ed_visits = mean(ed_visits)) %>% 
   ggplot(aes(x = weekday, 
              y = ed_visits)) + 
-  geom_beeswarm(aes(col = years_from_2017)) + 
+  geom_beeswarm(alpha = .4) + 
+  facet_wrap(~year) + 
   theme_light() +
   theme(panel.grid.minor = element_line(colour = "grey95"), 
-      panel.grid.major = element_line(colour = "grey95"))
-      
+        panel.grid.major = element_line(colour = "grey95"), 
+        axis.text.x = element_text(angle = 45, 
+                                   hjust = 1))
+
+
+df2.ed_visits_cleaned %>% 
+  ggplot(aes(x = weekday, 
+             y = ed_visits)) + 
+  geom_boxplot() + 
+  facet_wrap(~year) + 
+  theme_light() +
+  theme(panel.grid.minor = element_line(colour = "grey95"), 
+        panel.grid.major = element_line(colour = "grey95"),
+        axis.text.x = element_text(angle = 45, 
+                                   hjust = 1))
+
+
+df2.ed_visits_cleaned %>% 
+  ggplot(aes(x = year, 
+             y = ed_visits)) + 
+  geom_beeswarm() + 
+  facet_wrap(~weekday) + 
+  theme_light() +
+  theme(panel.grid.minor = element_line(colour = "grey95"), 
+        panel.grid.major = element_line(colour = "grey95"),
+        axis.text.x = element_text(angle = 45, 
+                                   hjust = 1))
+
+
+df2.ed_visits_cleaned %>% 
+  ggplot(aes(x = year, 
+             y = ed_visits)) + 
+  geom_boxplot() + 
+  facet_wrap(~weekday) + 
+  theme_light() +
+  theme(panel.grid.minor = element_line(colour = "grey95"), 
+        panel.grid.major = element_line(colour = "grey95"),
+        axis.text.x = element_text(angle = 45, 
+                                   hjust = 1))
+
+
+
+            

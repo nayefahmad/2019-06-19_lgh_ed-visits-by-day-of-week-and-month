3a34be2036a85a8bf88f004ee77a33c519276373
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2019-06-19 12:38:37 -0700

    Update setup-denodo_function.R

--- src/setup-denodo_function.R
+++ src/setup-denodo_function.R
@@ -10,16 +10,9 @@ setup_denodo <- function(){
       cnx <<- dbConnect(odbc::odbc(),
                        dsn = "cnx_denodo_spappcsta001")
       
-      vw_census <<- dplyr::tbl(cnx, 
-                              dbplyr::in_schema("publish", 
-                                                "census"))
       vw_eddata <<- dplyr::tbl(cnx, 
                               dbplyr::in_schema("publish", 
                                                 "emergency"))
-      vw_adtc <<- dplyr::tbl(cnx, 
-                            dbplyr::in_schema("publish", 
-                                              "admission_discharge"))
-      
       
       
 }
